94b5f0144a561d16a1a9899fd5917beb36a159fb
Israel Cm <isracat.32@gmail.com>
2021-07-06 15:41:37 -0400

    clean

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { PxpUiApp, configurePxpClient, Pxp, initI18n } from '@pxp-ui/core';
 import LoadingScreen from '@pxp-ui/components/LoadingScreen';
-import pagesTranslations from '@pxp-ui/common/modules/translate/pages';
 import configFile from './config';
-import { menu } from './cereals-ui/index';
-import LoginContainer from './cereals-ui/containers/LoginContainer';
-import pages from './cereals-ui/pages';
 import './index.scss';
 
 const config: any = configFile;
@@ -18,31 +14,11 @@ const renderApp = () => {
   if (!hasRendered) {
     hasRendered = true;
     ReactDOM.render(
-      <PxpUiApp
-        LoginContainer={LoginContainer}
-        pages={{ ...pages, ...pagesTranslations }}
-        i18n={i18n}
-      />,
+      <PxpUiApp i18n={i18n} />,
       document.getElementById('root'),
     );
   }
 };
 
-// const menu: any = [
-//   {
-//     uiId: 2,
-//     code: 'PXPLAN',
-//     text: 'CustomMenu Working',
-//     description: 'Language',
-//     component: 'EXA_MasterDetailExample',
-//     order: null,
-//     icon: null,
-//     parentId: 1,
-//     subsystem: 'PXP',
-//     children: [],
-//     type: 'leaf',
-//   },
-// ];
-const customMenu: any = menu;
 ReactDOM.render(<LoadingScreen />, document.getElementById('root'));
-configurePxpClient(Pxp, renderApp, customMenu);
+configurePxpClient(Pxp, renderApp);
